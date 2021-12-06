779c911cd3534d457450b2964e48c6d030c83b5c
TSB <tobishittabey@gmail.com>
2021-12-06 04:31:07 +0000

    [bernie] setTRAK : compulsory props, verifyCentral.

--- bernie/cloud-functions/functions/src/hooks/handlers/set/trak/setTRAK.ts
+++ bernie/cloud-functions/functions/src/hooks/handlers/set/trak/setTRAK.ts
@@ -1,43 +1,52 @@
 import { generateTRAKID, generateTRAKURI } from "../../generate";
+import { verifyCentralized } from "../../verify";
+
 import { constants } from "../../../../core";
 
 const { trak } = constants;
 
 export const setTRAK = ({ request, response }: any) => {
   const {
-    isrc,
-    type,
-    isNFT,
+    isrc /** NEED */,
+    type /** NEED */,
+    isNFT /** NEED */,
     trakIPO,
     trakART,
     trakAUDIO,
     trakVIDEO,
     subscriptions,
-    spotify,
+    spotify /** NEED */,
     apple_music,
     genius,
   } = request.body;
 
+  const forchainHash = "generateTRAKID()";
+  const solanaHash = "generateTRAKID()";
   const trakID = generateTRAKID();
   const trakURI = generateTRAKURI({ trak, type, trakID });
+  const centralized = [spotify, apple_music, genius];
+  const missingCentralizedPrimary: any[] = verifyCentralized(centralized);
 
   const trakToken: any = {
+    forchainHash,
+    solanaHash,
     trakID,
     trakURI,
+    subscriptions,
     isrc,
     type,
+    missingCentralizedPrimary,
     isNFT,
+    web: {
+      spotify,
+      apple_music,
+      genius,
+    },
     content: {
       trakIPO,
       trakART,
       trakAUDIO,
       trakVIDEO,
-    },
-    subscriptions,
-    web: {
-      spotify,
-      apple_music,
-      genius,
     },
   };
 
