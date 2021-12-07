83ab7e7984d136ae3ce3ddc123ee0e5d51032cab
TSB <tobishittabey@gmail.com>
2021-12-07 21:17:30 +0000

    [bernie-app] setup : app patern

---
+++ bernie/Bernie/src/internal/app/Bernie.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export const BernieApp = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: '#fff'}}>
+      <Text>Text</Text>
+    </SafeAreaView>
+  );
+};
