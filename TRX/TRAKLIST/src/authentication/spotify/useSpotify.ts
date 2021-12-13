2f0010e7db177a7ab605bbb71a9414771becbcb6
TSB <tobishittabey@gmail.com>
2021-12-13 16:43:06 +0000

    [traklist] register : spotify/google auth set up

---
+++ TRX/TRAKLIST/src/authentication/spotify/useSpotify.ts
@@ -0,0 +1,34 @@
+import React, {useEffect, useState, useContext} from 'react';
+import {authorize, refresh, revoke} from 'react-native-app-auth';
+
+export const useSpotify = () => {
+  const config = {
+    clientId: '29dec26a7f304507b4a9d9bcf0ef210b', // available on the app page
+    clientSecret: '1d27af3b5c4946c1a411657ca50490d0', // click "show client secret" to see this
+    redirectUrl: 'com.trxklist://oauthredirect/', // the redirect you defined after creating the app
+    scopes: [
+      'user-read-private',
+      'user-read-email',
+      'user-read-playback-state',
+      'user-library-modify',
+      'user-library-read',
+      'streaming',
+      'user-read-recently-played',
+      'user-follow-modify',
+      'user-top-read',
+      'playlist-modify-public',
+      'playlist-modify-private',
+      'user-follow-read',
+      'user-modify-playback-state',
+    ], // the scopes you need to access
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+      tokenEndpoint: 'https://accounts.spotify.com/api/token',
+    },
+  };
+
+  const authorizeSpotify = async () => await authorize(config);
+  return {
+    authorizeSpotify,
+  };
+};
--- TRX/TRAKLIST/src/elements/register-content-tab/RegisterConnectTabElement.tsx
+++ TRX/TRAKLIST/src/elements/register-content-tab/RegisterConnectTabElement.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Button} from 'react-native';
+import {useSpotify} from '../../authentication';
+import {useGoogle} from '../../authentication';
 
 export const RegisterConnectTabElement = () => {
+  const {authorizeSpotify} = useSpotify();
+  const {authorizeGoogle} = useGoogle();
   return (
     <View>
-      <Text></Text>
+      <Button title="spotify" onPress={() => authorizeSpotify()} />
+      <Button title="google" onPress={() => authorizeGoogle()} />
     </View>
   );
 };
