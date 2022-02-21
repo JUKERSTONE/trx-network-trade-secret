59476b2ce58189fff3c191aac29852907136462b
TSB <tobishittabey@gmail.com>
2022-02-21 05:25:34 +0000

    [BERNIE] routes : edits

--- bernie/cloud-functions/functions/src/index.ts
+++ bernie/cloud-functions/functions/src/index.ts
@@ -9,18 +9,18 @@ const {
   mineTRAKFunction,
   raffleFreeFunction,
   verifyDuplicateFunction,
-  getUserTRAKFunction,
+  // getUserTRAKFunction,
   getTRAKFunction,
   getTRAKBankFunction,
   exchangeTRAKFunction,
   requestNFTFunction,
   getNFTRequestsFunction,
-  getNFTRequestFunction,
+  // getNFTRequestFunction,
   scriptHasNFTFunction,
   setNFTFunction,
   purchaseNFTFunction,
-  getUserNFTFunction,
-  getNFTFunction,
+  // getUserNFTFunction,
+  // getNFTFunction,
   getUserWalletFunction,
   getArtistPortfolioFunction,
   appendNFTMerchandiseFunction,
@@ -34,33 +34,25 @@ app.post("/trak/verify/duplicate", verifyDuplicateFunction);
 app.post("/trak/append", auth, appendTRAKFunction);
 app.post("/trak/mine", mineTRAKFunction);
 app.get("/trak/raffle/:subscription/:username", auth, raffleFreeFunction);
-app.get("/user/:username/trak", auth, getUserTRAKFunction);
+// app.get("/user/:username/trak", auth, getUserTRAKFunction);
 app.get("/trak/:id", getTRAKFunction);
 app.get("/trak", auth, getTRAKBankFunction);
 app.get(
   "/trak/exchange/:bought/:sold/:username",
   auth,
-  /**rename*/ exchangeTRAKFunction
+  /**params to body*/ exchangeTRAKFunction
 );
-app.post("/trak/nft/request", auth, /**rename*/ requestNFTFunction);
-app.get("/trak/nft/requests", /**rename */ getNFTRequestsFunction);
-app.get("/trak/nft/request/:trakID", /**rename */ getNFTRequestFunction);
-app.get("/script/trak/hasNFT", auth, scriptHasNFTFunction);
+app.post("/nft/request", auth, requestNFTFunction);
+app.get("/nft/requests", auth, getNFTRequestsFunction);
+// app.get("/trak/nft/request/:trakID", auth, /**rename */ getNFTRequestFunction);
+app.get("/script/trak/hasNFT", scriptHasNFTFunction);
 app.post("/nft", auth, setNFTFunction);
-app.get("/nft/purchase/:nftID/:username", auth, purchaseNFTFunction);
-app.get("/user/:username/nft", auth, getUserNFTFunction);
-app.get("/nft/:id", auth, getNFTFunction);
+app.get("/nft/:nftID/purchase/:username", auth, purchaseNFTFunction);
+// app.get("/user/:username/nft", auth, getUserNFTFunction);
+// app.get("/nft/:id", auth, getNFTFunction);
 app.get("/user/:username/wallet", auth, getUserWalletFunction);
 app.get("/artist/:userID/portfolio", auth, getArtistPortfolioFunction);
-app.post(
-  "/nft/merchandise/add",
-  auth,
-  /** rename*/ appendNFTMerchandiseFunction
-);
-app.get(
-  "/nft/merchandise/:nftID",
-  auth,
-  /** rename */ getNFTMerchandiseFunction
-);
+app.post("/nft/merchandise", auth, appendNFTMerchandiseFunction);
+app.get("/nft/:nftID/merchandise", auth, getNFTMerchandiseFunction);
 
 exports.BERNIE = functions.region("europe-west1").https.onRequest(app);
