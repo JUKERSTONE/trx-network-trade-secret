4f02c73fd623cc84dc2704c0296d9699b1cbeafc
TSB <tobishittabey@gmail.com>
2022-03-06 13:30:24 +0000

    [TLT] style : adverts

--- TLT/TRAKLITE/src/7.elements/swipe-advert/SwipeAdvert.tsx
+++ TLT/TRAKLITE/src/7.elements/swipe-advert/SwipeAdvert.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
-import * as Animatable from 'react-native-animatable';
+import React from "react";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import * as Animatable from "react-native-animatable";
 
 interface TSwipeAdvert {
   card?: any;
-  mode: 'snapchat' | 'traklist' | 'google';
+  mode: "snapchat" | "traklist" | "google";
 }
 
-export const SwipeAdvert: React.FC<TSwipeAdvert> = ({card, mode}) => {
+export const SwipeAdvert: React.FC<TSwipeAdvert> = ({ card, mode }) => {
   return (
-    <Animatable.View animation={'bounceIn'}>
+    <Animatable.View animation={"bounceIn"}>
       <View
         style={[
           styles.card,
           {
-            backgroundColor: 'grey',
-            alignItems: 'center',
-            justifyContent: 'center',
+            backgroundColor: "grey",
+            alignItems: "center",
+            justifyContent: "center",
           },
-        ]}>
-        <Text style={{color: 'whitesmoke'}}>SHMONEY TIME</Text>
-        <Text style={{color: 'whitesmoke'}}>{mode}</Text>
+        ]}
+      >
+        <Text style={{ color: "whitesmoke", fontWeight: "bold" }}>
+          BILLBOARD
+        </Text>
+        {/* <Text style={{ color: "whitesmoke" }}>{mode}</Text> */}
+        <Text style={{ color: "whitesmoke" }}>
+          Email App support for Enquiries.
+        </Text>
+        <Text style={{ color: "whitesmoke", marginTop: 5 }}>
+          info@tsb.media
+        </Text>
       </View>
     </Animatable.View>
   );
@@ -32,9 +41,9 @@ const styles = StyleSheet.create({
   },
   card: {
     // flex: 0.85,
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 400,
-    width: '100%',
+    width: "100%",
     borderRadius: 25,
     marginTop: 20,
   },
