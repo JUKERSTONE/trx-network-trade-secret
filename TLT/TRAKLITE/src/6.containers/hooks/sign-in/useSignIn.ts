4fe72d74d55312dbe74455af8b4920f502583ac9
TSB <tobishittabey@gmail.com>
2022-05-04 02:56:11 +0100

    [TLT] feat : Apple Music integration

--- TLT/TRAKLITE/src/6.containers/hooks/sign-in/useSignIn.ts
+++ TLT/TRAKLITE/src/6.containers/hooks/sign-in/useSignIn.ts
@@ -1,10 +1,13 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-import {SignInState} from './types';
-import {store} from '../../../3.stores';
-import * as actions from '../../../3.stores';
-import {updateSignInState, signInState} from './handlers';
-import {signIn, signInWithSpotify} from '../../../2.auth';
+import { SignInState } from "./types";
+import { store } from "../../../3.stores";
+import * as actions from "../../../3.stores";
+import { updateSignInState, signInState } from "./handlers";
+import { signIn, signInWithSpotify } from "../../../2.auth";
+import axios from "axios";
+// @ts-ignore
+import AppleMusic from "@bouncyapp/react-native-apple-music";
 
 export const useSignIn = (navigation: any) => {
   const [signInForm, setSignInForm] = useState<SignInState>(signInState);
@@ -21,10 +24,10 @@ export const useSignIn = (navigation: any) => {
 
     if (newData.success) {
       setIsLoadingTraklist(false);
-      store.dispatch(actions.USER_LOGGED_IN('log user in.', newData));
+      store.dispatch(actions.USER_LOGGED_IN("log user in.", newData));
     } else {
       setIsLoadingTraklist(false);
-      alert('User details not found. Try again');
+      alert("User details not found. Try again");
     }
   };
 
@@ -32,14 +35,14 @@ export const useSignIn = (navigation: any) => {
     setIsLoadingSpotify(true);
     const newData = await signInWithSpotify();
     console.log(
-      '🚀 ~ file: useSignIn.ts ~ line 34 ~ handleSignInWithSpotify ~ newData',
-      newData,
+      "🚀 ~ file: useSignIn.ts ~ line 34 ~ handleSignInWithSpotify ~ newData",
+      newData
     );
     // if empty
 
     if (newData.success) {
       setIsLoadingSpotify(false);
-      store.dispatch(actions.USER_LOGGED_IN('log user in.', newData.data));
+      store.dispatch(actions.USER_LOGGED_IN("log user in.", newData.data));
     } else {
       setIsLoadingSpotify(false);
       alert(newData.data);
@@ -47,11 +50,35 @@ export const useSignIn = (navigation: any) => {
   };
 
   const handleSignInWithApple = async () => {
-    alert('Apple Music authentication coming soon');
+    const AppleMusicKeyId = "MBVSJA2QBU";
+    const AppleMusicTeamId = "3J39XKJXT5";
+    const AppleMusicPrivateKey =
+      "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgDnVAe5C0dO1ouzHu\nfEJpHLfb4KsL3kAa5JVOLdcoiu+gCgYIKoZIzj0DAQehRANCAATvYaFa0e/LgWb2\noIwX1OOpBilYle616YJmPDhgNLvtb4YoiDCqIEdSdgcRzEM5rnnLFwc1evaYPyOb\neX8ghAKf\n-----END PRIVATE KEY-----";
+
+    await AppleMusic.initialize(
+      AppleMusicKeyId,
+      AppleMusicTeamId,
+      AppleMusicPrivateKey
+    );
+    const test = await AppleMusic.login()
+      .then((response: any) => {
+        console.log(
+          "🚀 ~ file: useSignIn.ts ~ line 77 ~ test ~ response",
+          response
+        );
+      })
+      .catch((err: any) => {
+        alert(err);
+      });
+    console.log(
+      "🚀 ~ file: useSignIn.ts ~ line 77 ~ handleSignInWithApple ~ test",
+      test
+    );
+    await AppleMusic.getUserMusicToken();
   };
 
   const handleAuthNavigation = () => {
-    navigation.navigate('REGISTER.');
+    navigation.navigate("REGISTER.");
   };
 
   return {
