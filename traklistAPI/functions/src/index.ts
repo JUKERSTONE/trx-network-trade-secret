5e51634d4ef9768e389d44d523a7884f1745bd5a
TSB <tobishittabey@gmail.com>
2022-08-20 14:43:22 +0100

    [API] wip : token swap & refresh

--- traklistAPI/functions/src/index.ts
+++ traklistAPI/functions/src/index.ts
@@ -1,48 +1,13 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import { useCloudFunctions } from "./hooks";
-import { auth } from "./core";
 
-const {
-  setTLTTrendingFunction,
-  getTLTTrendingFunction,
-  setTLTNewsFunction,
-  getTLTNewsFunction,
-  setTrakFunction,
-  verifyDuplicateTrakFunction,
-  // setTrakRaffleFunction,
-  getTrakFunction,
-  getTrakBankFunction,
-  // exchangeTrakFunction,
-  requestNFTFunction,
-  getNFTRequestsFunction,
-  setNFTFunction,
-  purchaseNFTFunction,
-  getUserWalletFunction,
-  getArtistPortfolioFunction,
-} = useCloudFunctions();
+const { handleSwapTokenFunction, handleRefreshTokenFunction } =
+  useCloudFunctions();
 
 export const app = express();
 
-app.post("/trx_00/trak", auth, setTrakFunction);
-app.post("/trx_00/trak/verify/duplicate", verifyDuplicateTrakFunction);
-// app.get(
-//   "/traklist/user/trak/trx_00/raffle/:subscription",
-//   auth,
-//   setTrakRaffleFunction // come back after platforms
-// );
-app.get("/trx_00/trak/:trakId", getTrakFunction);
-app.get("/trx_00/trak", auth, getTrakBankFunction);
-// app.post("/trx_00/trak/exchange", auth, exchangeTrakFunction);
-app.post("/trx_00/nft/request", auth, requestNFTFunction); // come back after platforms
-app.get("/trx_00/nft/requests", auth, getNFTRequestsFunction); // come back after platforms
-app.post("/trx_00/nft", auth, setNFTFunction); // come back after platforms
-app.post("/trx_00/nft/:nftID/purchase", auth, purchaseNFTFunction);
-app.get("/traklist/user/wallet", auth, getUserWalletFunction);
-app.get("/traklist/artist/portfolio", auth, getArtistPortfolioFunction);
-app.post("/traklite/admin/trending", auth, setTLTTrendingFunction);
-app.get("/traklite/admin/trending", getTLTTrendingFunction);
-app.post("/traklite/admin/news", auth, setTLTNewsFunction);
-app.get("/traklite/admin/news", getTLTNewsFunction);
+app.post("/spotify/swap", handleSwapTokenFunction);
+app.post("/spotify/refresh", handleRefreshTokenFunction);
 
 exports.TRAKLIST = functions.region("europe-west1").https.onRequest(app);
