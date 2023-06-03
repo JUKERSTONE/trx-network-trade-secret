eecdd1e8a4343d648d0fdda8e94a4f8cbc1572c0
TSB <tobishittabey@gmail.com>
2023-06-03 12:46:10 +0100

    [TLT] fix : init prod images

--- TLT/LiteList/src/elements/product/Product.tsx
+++ TLT/LiteList/src/elements/product/Product.tsx
@@ -5,15 +5,18 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
+  ScrollView,
+  FlatList,
 } from 'react-native';
 import React, {useRef, useEffect, useState} from 'react';
 import {VHeader, Body, Caption} from '../typography';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {ProgressBar, Colors} from 'react-native-paper';
+import Entypo from 'react-native-vector-icons/Entypo';
+import {ProgressBar, Colors, ActivityIndicator} from 'react-native-paper';
 // @ts-ignore
 import ParallaxScrollView from 'react-native-parallax-scroll-view';
 import LinearGradient from 'react-native-linear-gradient';
 import Carousel from 'react-native-snap-carousel';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 export const ProductElement = ({
   route: {
@@ -21,72 +24,155 @@ export const ProductElement = ({
   },
   handlePurchaseProduct,
 }: any) => {
+  const [imagesInit, setImagesInit] = useState(false);
+  const [selectedSize, setSelectedSize] = useState(item.sizes[0]);
+  const [quantity, setQuantity] = useState(0);
   console.log('🚀 ~ file: Product.tsx:11 ~ ProductElement ~ item:', item);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImagesInit(true);
+    }, 1000);
+  });
+
   return (
     <ParallaxScrollView
       backgroundColor={'#1a1a1a'}
       parallaxHeaderHeight={300}
       stickyHeaderHeight={150}
       renderForeground={() => (
-        <LinearGradient colors={['#1a1a1a', '#000']}>
-          <Carousel
-            // layout={'stack'}
-            layoutCardOffset={30}
-            data={item.images}
-            renderItem={({item}: any) => {
-              console.log('🚀 ~ file: Product.tsx:55 ~ item:', item);
+        <View>
+          {imagesInit ? (
+            <Carousel
+              // layout={'stack'}
+              // layoutCardOffset={30}
+              data={item.images}
+              renderItem={({item}: any) => {
+                console.log('🚀 ~ file: Product.tsx:55 ~ item:', item);
 
-              return (
-                <Image
-                  style={{
-                    height: '100%',
-                    width: '100%',
-                    marginTop: 3,
-                    borderRadius: 8,
-                  }}
-                  source={{
-                    uri: item,
-                  }}
-                />
-              );
-            }}
-            sliderWidth={Dimensions.get('screen').width}
-            itemWidth={Dimensions.get('screen').width * 0.75}
-          />
-        </LinearGradient>
+                return (
+                  <Image
+                    style={{
+                      height: 250,
+                      width: '100%',
+                      marginTop: 3,
+                      borderRadius: 8,
+                    }}
+                    source={{
+                      uri: item,
+                    }}
+                  />
+                );
+              }}
+              sliderWidth={Dimensions.get('screen').width}
+              itemWidth={Dimensions.get('screen').width * 0.75}
+            />
+          ) : (
+            <View
+              style={{
+                height: 250,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <ActivityIndicator size="large" color="primary" />
+            </View>
+          )}
+        </View>
       )}>
-      <View
-        style={{
-          backgroundColor: '#1a1a1a',
-          height: '100%',
-          padding: 10,
-        }}>
-        <View style={{padding: 4}}>
-          <VHeader type="three" color="#cecece" text={item.product} />
+      <View style={{margin: 15}}>
+        <View style={{marginBottom: 10}}>
+          <VHeader type="three" color="#232323" text={item.product} />
           <Caption type="one" color="#cecece" text={item.brand} />
-          <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-            <Caption type="one" color="#cecece" text={`£${item.price}`} />
-
-            <TouchableOpacity onPress={handlePurchaseProduct}>
+        </View>
+        <Text style={{color: '#1a1a1a'}}>{item.description}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 10,
+            justifyContent: 'space-between',
+          }}>
+          {item.sizes.map((size: any) => (
+            <TouchableOpacity onPress={() => setSelectedSize(size)}>
               <View
                 style={{
-                  height: 30,
-                  width: 100,
-                  backgroundColor: 'green',
-                  borderRadius: 8,
-                  alignItems: 'center',
+                  height: 35,
+                  width: 35,
+                  backgroundColor: selectedSize === size ? 'green' : '#232323',
                   justifyContent: 'center',
+                  alignItems: 'center',
+                  margin: 5,
+                  borderRadius: 10,
                 }}>
-                <Text style={{color: '#fff'}}>BUY</Text>
+                <Text style={{color: '#ffff'}}>{size}</Text>
               </View>
             </TouchableOpacity>
-          </View>
+          ))}
 
-          <View style={{margin: 15}}>
-            <Text style={{color: '#fff'}}>{item.description}</Text>
-            {/* <Text style={{color: '#fff'}}>{item.sizes}</Text> */}
-            {/* <Text style={{color: '#fff'}}>{item.details}</Text> */}
+          <TouchableOpacity
+            onPress={() => setQuantity(quantity + 1)}
+            style={{
+              width: 80,
+              height: '100%',
+              backgroundColor: '#cecece',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              borderRadius: 15,
+              padding: 5,
+            }}>
+            <View style={{marginRight: 5}}>
+              <VHeader type="four" color="#1a1a1a" text={quantity + ''} />
+            </View>
+            <VHeader type="three" color="#1a1a1a" text={'+'} />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity onPress={handlePurchaseProduct}>
+          <View
+            style={{
+              height: 60,
+              width: '100%',
+              backgroundColor: 'green',
+              margin: 10,
+              alignItems: 'center',
+              flexDirection: 'row',
+              borderRadius: 10,
+            }}>
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <Entypo name="shopping-basket" size={35} color={'#fff'} />
+            </View>
+            <View style={{flex: 2, paddingLeft: 15}}>
+              <VHeader type="four" color="#fff" text={'Go to Checkout'} />
+            </View>
           </View>
+        </TouchableOpacity>
+
+        <View>
+          <FlatList
+            data={Object.keys(item.details)}
+            renderItem={obj => {
+              console.log('🚀 ~ file: Product.tsx:140 ~ detail:', obj.item);
+
+              return (
+                <View style={{padding: 5}}>
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: 'black',
+                      padding: 5,
+                      borderRadius: 7,
+                    }}>
+                    <Text style={{color: '#fff'}}>{obj.item}</Text>
+                  </View>
+                  <View style={{flex: 2, marginTop: 10}}>
+                    <Text style={{color: '#232323', textAlign: 'right'}}>
+                      {item.details[obj.item]}
+                    </Text>
+                  </View>
+                </View>
+              );
+            }}
+          />
         </View>
       </View>
     </ParallaxScrollView>
