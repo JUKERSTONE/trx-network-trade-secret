9c7d87d8547ccde3f9b12d78ad3895be66fabbd9
TSB <tobishittabey@gmail.com>
2023-10-27 16:44:35 +0100

    API: fix: trx-04 path

--- API_PROD/functions/src/hooks/handlers/set/trx_00/trak/setTrak.ts
+++ API_PROD/functions/src/hooks/handlers/set/trx_00/trak/setTrak.ts
@@ -22,13 +22,14 @@ export const setTrak = (req: any, res: any) => {
         })
         .catch((error) => res.json("Error - Could not set TRAK"));
     case "trx-04":
+      const ytid = TRAK.trak.youtube.url.split("=")[1];
       return db
-        .doc(`/trx-04/trx:04:${TRAK.trak.youtube.url.split("=")[1]}`)
+        .doc(`/trx-04/trx:04:${ytid}/`)
         .set({
           artist: TRAK.trak.artist,
           title: TRAK.trak.title,
           serializedTrak: JSON.stringify({ protocol, TRAK }),
-          ytid: TRAK.trak.youtube.url.split("=")[1],
+          ytid,
         })
         .then(() => {
           return res.json({
